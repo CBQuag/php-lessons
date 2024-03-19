41a9f3cc950eb7b1ec76203b20dcb79570f48a06
Conrad Quagliaroli <conradbquag@gmail.com>
2024-03-19 15:57:29 -0400

    switch case

--- php-practice.php
+++ php-practice.php
@@ -34,6 +34,9 @@ $people_single = [
     'sally' => ['name' => 'sally', 'age' => 32],
     'bob' => ['name' => 'bob', 'age' => 20]
 ];
+
+//if you arbitrarily add ... 3=>'whatever', then the array continues from that index
+//ignoring the count
 echo $people_single['sally']['name'];
 echo '</br>';
 
@@ -48,8 +51,40 @@ foreach ($people_single as $key => $p) {
     echo $key . '-' . $p['age'] . '</br>';
 }
 
+$arr1 = [];
+if (empty ($arr1))
+    echo "arr1 is empty";
+echo '</br>';
+
 var_dump($message, $pi);
 echo '</br>';
 var_dump($arr["blurst"]);
 echo '</br>';
 
+$month = 1;
+while ($month <= 12) {
+    echo $month;
+    echo '</br>';
+    $month++;
+}
+for ($x = 0; $x <= 5; $x++) {
+    echo $x;
+}
+echo '</br>';
+
+$day = 'tue';
+
+switch ($day) {
+    case 'mon':
+        echo 'its monday';
+        break;
+    case 'tue':
+        echo 'its tuesday';
+        break;
+    default:
+        echo 'not a day';
+        break;
+}
+
+//without breaks, switches run everything past the true condition
+echo '</br>';
